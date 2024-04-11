282b38ad8de7bbc57cd55d85d2a93c3ddbd06aaa
Latteholic <o.smirnova@pvp.co.jp>
2024-04-12 01:13:44 +0900

    Add plaer movements - go right/left, jump

---
+++ First-app/Assets/CharacterMovements.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovements : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public float speed = 10;
+    Vector2 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        //Move(transform);
+        move = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(move.x * speed * Time.deltaTime, move.y * speed * Time.deltaTime);
+    }
+
+    //    public void Move(Transform transform)
+    //    {
+
+    //       //if (Input.GetKeyDown(KeyCode.LeftArrow) && CharacterRigidBody.position.x >= -9.5)
+    //       // {
+    //       //     CharacterRigidBody.velocity = Vector2.left * 7;
+    //       // } else if (Input.GetKeyDown(KeyCode.LeftArrow) && CharacterRigidBody.position.x >= -9.5)
+    //       // {
+    //       //     CharacterRigidBody.velocity = Vector2.zero;
+    //       //     //CharacterRigidBody.position.x = -9.5;
+    //       // }
+
+    //       // if (Input.GetKeyDown(KeyCode.RightArrow) && CharacterRigidBody.position.x <= -10.5)
+    //       // {
+    //       //     CharacterRigidBody.velocity = Vector2.right * 7;
+    //       // }
+    //       // else if (Input.GetKeyDown(KeyCode.RightArrow) && CharacterRigidBody.position.x <= -10.5)
+    //       // {
+    //       //     CharacterRigidBody.velocity = Vector2.zero;
+    //       // }
+
+    //       // if (Input.GetKeyDown(KeyCode.Space))
+    //       // {
+    //       //     CharacterRigidBody.velocity = new Vector2(CharacterRigidBody.velocity.y);
+    //       // }
+
+    //    }
+
+
+}
