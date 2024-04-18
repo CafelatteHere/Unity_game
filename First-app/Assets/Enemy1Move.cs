1bba7560f0a54f4ed5dec3676e68c1ebde84634e
Latteholic <o.smirnova@pvp.co.jp>
2024-04-18 15:00:28 +0900

    In-process adding collision

--- First-app/Assets/Enemy1Move.cs
+++ First-app/Assets/Enemy1Move.cs
@@ -4,18 +4,19 @@ using UnityEngine;
 
 public class Enemy1Move : MonoBehaviour
 {
-    [SerializeField] private Rigidbody2D rbEnemy2;
+    //TODO Make enemies not to collide with each other
+    [SerializeField] private Rigidbody2D rbEnemy1;
     [SerializeField] private float speed;
     [SerializeField] private float timer;
 
     // Start is called before the first frame update
     void Awake()
     {
-        rbEnemy2 = GetComponent<Rigidbody2D>();
+        rbEnemy1 = GetComponent<Rigidbody2D>();
         speed = 3;
-        rbEnemy2.transform.position = new Vector2(7.4f, -2.3f);
+        rbEnemy1.transform.position = new Vector2(7.4f, -2.3f);
         timer = 0.0f;
-        rbEnemy2.velocity = new Vector2(-speed, 0);
+        rbEnemy1.velocity = new Vector2(-speed, 0);
     }
 
     // Update is called once per frame
@@ -23,16 +24,32 @@ public class Enemy1Move : MonoBehaviour
     {
         timer = timer + Time.deltaTime;
 
-        if (timer > 4.9f && rbEnemy2.velocity[0] < 0)
+        if (timer > 4.9f && rbEnemy1.velocity[0] < 0)
         {
-            rbEnemy2.velocity = new Vector2(speed, 0);
+            rbEnemy1.velocity = new Vector2(speed, 0);
             timer = 0.0f;
         }
 
-        if (timer > 4.9f && rbEnemy2.velocity[0] > 0)
+        if (timer > 4.9f && rbEnemy1.velocity[0] > 0)
         {
-            rbEnemy2.velocity = new Vector2(-speed, 0);
+            rbEnemy1.velocity = new Vector2(-speed, 0);
             timer = 0.0f;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            Debug.Log("collision!");
+            rbEnemy1.velocity = new Vector2(0, 0);
+            Debug.Log("awake!");
+        }
+    }
+
+    IEnumerator WaitAndAwake()
+    {
+        yield return new WaitForSeconds(3f);
+        Debug.Log("Awake!");
     }
 }
