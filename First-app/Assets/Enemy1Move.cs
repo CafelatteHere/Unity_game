bc2971861a33f06304aee1e33c5b7ccf731926ed
Latteholic <o.smirnova@pvp.co.jp>
2024-04-26 17:08:45 +0900

    adding Raycast to Enemies1, 2 - currently in progress.

--- First-app/Assets/Enemy1Move.cs
+++ First-app/Assets/Enemy1Move.cs
@@ -7,44 +7,75 @@ public class Enemy1Move : MonoBehaviour
     //[SerializeField] private Rigidbody2D rbEnemy1;
     [SerializeField] private float speed = 3;
     [SerializeField] private float flipTime = 4.9f;
+    //TODO change startPoint for Enemy2;
     [SerializeField] private Vector2 startPoint = new Vector2(7.4f, -2.3f);
+    [SerializeField] private Vector2 boxSize;
+    [SerializeField] private float groundCheckDistance = 0.1f;
 
-    private Rigidbody2D rbEnemy1;
- 
+    private Rigidbody2D rbEnemy;
     private float timer = 0.0f;
+    private bool isGrounded;
+    private LayerMask groundLayer;
 
     // Start is called before the first frame update
     void Awake()
     {
         //Rigidbody2D rbEnemy1 makes it to be a new var 
-        rbEnemy1 = GetComponent<Rigidbody2D>();
-        Debug.Log(rbEnemy1);
-        rbEnemy1.transform.position = startPoint;
+        rbEnemy = GetComponent<Rigidbody2D>();
+        Debug.Log(rbEnemy);
+        rbEnemy.transform.position = startPoint;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer = timer + Time.deltaTime;
-        rbEnemy1.velocity = new Vector2(speed, rbEnemy1.velocity.y);
+        checkGround();
+        //timer = timer + Time.deltaTime;
+        rbEnemy.velocity = new Vector2(speed, rbEnemy.velocity.y);
+        
+        //isGrounded = checkGround();
+        //if (isGrounded == false)
+        //{
+        //    speed *= -1;
+        //}
+        //if (timer > flipTime)
+        //{
+        //    speed += -1;
+        //    timer = 0;
+        //}
 
-        if (timer > flipTime)
+        //if (timer > flipTime && rbEnemy.velocity.x <= 0)
+        //{
+        //    rbEnemy.velocity = new Vector2(speed, rbEnemy.velocity.y);
+        //    timer = 0.0f;
+        //}
+
+        //if (timer > flipTime && rbEnemy.velocity.x > 0)
+        //{
+        //    rbEnemy.velocity = new Vector2(-speed, rbEnemy.velocity.y);
+        //    timer = 0.0f;
+        //}
+    }
+
+    private void checkGround()
+    {
+        RaycastHit2D onGround = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, groundLayer);
+        Debug.Log(onGround);
+        //if (Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, castDistance, groundLayer))
+        if (onGround == false)
         {
-            speed += -1;
-            timer = 0;
+            Debug.Log("hitting ground");
+           // return onGround.collider != null;
+            speed *= -1;
+
+
+
         }
+    }
 
-        //if (timer > flipTime && rbEnemy1.velocity.x <= 0)
-        //{
-        //    rbEnemy1.velocity = new Vector2(speed, rbEnemy1.velocity.y);
-        //    timer = 0.0f;
-        //}
-
-        //if (timer > flipTime && rbEnemy1.velocity.x > 0)
-        //{
-        //    rbEnemy1.velocity = new Vector2(-speed, rbEnemy1.velocity.y);
-        //    timer = 0.0f;
-        //}
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position - transform.up * groundCheckDistance, boxSize);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -52,7 +83,7 @@ public class Enemy1Move : MonoBehaviour
         if (collision.gameObject.layer == 8)
         {
             Debug.Log("collision!");
-            rbEnemy1.velocity = new Vector2(0, 0);
+            rbEnemy.velocity = new Vector2(0, 0);
             StartCoroutine(WaitAndAwake());
             Debug.Log("awake!");
         }
