df0201aec922037345963ea850d05442af4566c9
Olga-pvp <156168010+Olga-pvp@users.noreply.github.com>
2024-09-29 22:09:57 +0900

    -upated drops spawn logic

--- First-app/Assets/Scripts/DropMovement.cs
+++ First-app/Assets/Scripts/DropMovement.cs
@@ -7,29 +7,20 @@ public class DropMovement : MonoBehaviour
 
     SpawnObstacles spawnObstacles;
 
-    // Start is called before the first frame update
     void Start()
     {
         spawnObstacles = FindObjectOfType<SpawnObstacles>();
 
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+   
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log("collision happened");
-        Debug.Log(collision.gameObject);
         if (collision.gameObject.layer == LayerMask.NameToLayer("groundLayer"))
         {
-            Debug.Log(gameObject);
            
             gameObject.SetActive(false);
-            spawnObstacles.pooledDrops2.Enqueue(gameObject);
+            spawnObstacles.pooledDrops.Enqueue(gameObject); 
         }
     }
 }
--- First-app/Assets/Scripts/SpawnObstacles.cs
+++ First-app/Assets/Scripts/SpawnObstacles.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,37 +8,65 @@ public class SpawnObstacles : MonoBehaviour
     [SerializeField] GameObject drop;
     [SerializeField] private Transform[] spawnPoints;
     [SerializeField] private Vector2 LaunchSpeed;
-    [SerializeField] int stopTimer;
+    [SerializeField] float startTimer = 4;
+    [SerializeField] float stopTimer = 4;
+    [SerializeField] float startTimerMax = 4;
+    [SerializeField] float stopTimerMax = 4;
+    [SerializeField] float oneDropsRowSpawnTimer = 0.5f;
+    [SerializeField] float oneDropsRowSpawnTimerMax = 0.5f;
 
     private Rigidbody2D dropRb;
     private bool isPaused;
-    public List<GameObject> pooledDrops = new List<GameObject>();
-    public Queue<GameObject> pooledDrops2 = new Queue<GameObject>();
-    private int amountToPool = 70;
-    // Start is called before the first frame update
+    public Queue<GameObject> pooledDrops = new Queue<GameObject>();
+    private int amountToPool = 30;
+ 
     void Start()
+    {
+        AddToPool(drop);
+    }
+
+    private void AddToPool(GameObject objectToSpawn)
     {
         for (int i = 0; i < amountToPool; i++)
         {
-            GameObject newDrop = Instantiate(drop);
+            GameObject newDrop = Instantiate(objectToSpawn);
             newDrop.SetActive(false);
-            pooledDrops2.Enqueue(newDrop);
+            pooledDrops.Enqueue(newDrop);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        HandleSpawnDrops();
+        stopTimer -= Time.deltaTime;
+        if (stopTimer < 0)
+        {
+   
+            startTimer -= Time.deltaTime;
+            oneDropsRowSpawnTimer -= Time.deltaTime;
+            if (oneDropsRowSpawnTimer < 0)
+            {
+                SpawnDrops();
+                oneDropsRowSpawnTimer = oneDropsRowSpawnTimerMax;
+                
+            }
+            
+            if (startTimer < 0)
+            {
+                stopTimer = stopTimerMax;
+                startTimer = startTimerMax;
+            }
+        }
+       
     }
 
     public GameObject GetPooledDrop()
     {
-       foreach (GameObject drop in pooledDrops2)
+       foreach (GameObject drop in pooledDrops)
         {
             if (!drop.activeInHierarchy)
             {
-               GameObject newDrop = pooledDrops2.Dequeue();
+               GameObject newDrop = pooledDrops.Dequeue();
                return newDrop;
             }
         }
@@ -45,38 +74,18 @@ public class SpawnObstacles : MonoBehaviour
         return null;
     }
 
-    void HandleSpawnDrops()
+   
+  private void SpawnDrops()
     {
-       if (stopTimer > 0)
-        {
-            stopTimer -= 1;
-            //SpawnDrops();
-        }
-
-       else if (stopTimer == 0 )
-        {
-
-            isPaused = true;
-        }
-        StartCoroutine(SpawnDrops());
-
-    }
-
-    IEnumerator SpawnDrops()
-    {
-        if (!isPaused)
-        {
         foreach (Transform spawnPoint in spawnPoints) {
                     Quaternion spawnRotation = Quaternion.identity;
                 //Transform randomSpawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length - 1)];
                 // GameObject newDrop = Instantiate(drop, spawnPoint.position, spawnRotation);
 
                 GameObject newDrop = GetPooledDrop();
-                Debug.Log("check point 1");
 
                 if (newDrop != null)
                 {
-                    Debug.Log("check point 2");
                     newDrop.SetActive(true);
                     newDrop.transform.position = spawnPoint.position;
                     var direction = new Vector2(0, -1);
@@ -84,16 +93,14 @@ public class SpawnObstacles : MonoBehaviour
                     dropRb = newDrop.GetComponent<Rigidbody2D>();
                     dropRb.AddForce(LaunchSpeed * direction, ForceMode2D.Impulse);
                 }
-                    
-                }
-                yield return new WaitForSecondsRealtime(7f);
-        }
-        else
-        {
-            yield return new WaitForSecondsRealtime(4f);
-            stopTimer = 20;
-            isPaused = false;
-        }
+                  else
+            {
+                Debug.Log("not enough drops, making new ones!");
+                newDrop = Instantiate(drop);
+                newDrop.SetActive(false);
+                pooledDrops.Enqueue(newDrop);
+            }
+                }        
     }
 
 }
